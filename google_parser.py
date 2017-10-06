f1f7e76b49f5cd2cfe17f207b51d59673b2f532f
kormyshov <kormyshov@gmail.com>
2017-10-06 14:26:49 +0300

    Fixed schema

--- google_parser.py
+++ google_parser.py
@@ -3,9 +3,9 @@ import json
 
 
 def get_path(tag):
-    path = tag.name + str(tag.attrs)
+    path = tag.name + " <@> " + json.dumps(tag.attrs)
     for parent in tag.parents:
-        path = parent.name + str(parent.attrs) + "." + path
+        path = parent.name + " <@> " + json.dumps(parent.attrs) + " <$> " + path
     return path
 
 
@@ -13,10 +13,10 @@ def parse_document(tag):
     document = dict()
     document["type"] = "SEARCH_RESULT"
     document["alignment"] = "LEFT"
-    document["page_url"] = get_path(tag.h3.a) + "['href']"
-    document["title"] = get_path(tag.h3.a) + ".string"
-    document["snippet"] = get_path(tag.find("span", {"class": "st"})) + ".string"
-    document["view_url"] = get_path(tag.find("cite")) + ".string"
+    document["page-url"] = get_path(tag.h3.a) + " <@> ['href']"
+    document["title"] = get_path(tag.h3.a) + " <@> string"
+    document["snippet"] = get_path(tag.find("span", {"class": "st"})) + " <@> strings"
+    document["view-url"] = get_path(tag.find("cite")) + " <@> string"
     return document
 
 
@@ -27,10 +27,10 @@ def parse_wizard_image(tag):
     wizard["media-links"] = list()
     for link in tag.find("div", {"class": "rg_ul"}).contents:
         if type(link) == element.Tag:
-            wizard["media-links"].append({"url": get_path(link.contents[1]) + ".string['ou']"})
+            wizard["media-links"].append({"url": get_path(link.contents[1]) + " <@> inner <@> ['ou']"})
     wizard["wizard-type"] = "WIZARD_IMAGE"
-    wizard["page-url"] = get_path(tag.find("a")) + "['href']"
-    wizard["title"] = get_path(tag.find("a")) + ".string"
+    wizard["page-url"] = get_path(tag.find("a")) + " <@> ['href']"
+    wizard["title"] = get_path(tag.find("a")) + " <@> string"
     return wizard
 
 
@@ -50,8 +50,11 @@ def parse_page(file_handle):
 
 
 def main():
-    file = open("./google/2.html", "r")
-    parse_page(file)
+    with open("./google/3.html", "r") as file:
+        dump = parse_page(file)
+
+    with open("schema.txt", "w") as fo:
+        fo.write(dump)
 
 
 if __name__ == "__main__":
