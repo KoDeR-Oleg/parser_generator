455461dca9ab4352c6b5bc792dce4413a93244c3
kormyshov <kormyshov@gmail.com>
2017-10-12 23:34:21 +0300

    Add get_substitution

--- src/Markup.py
+++ src/Markup.py
@@ -1,3 +1,6 @@
+from lxml import html
+
+
 class FullPath(object):
     def __init__(self, xpath, attr):
         self.xpath = xpath
@@ -17,6 +20,24 @@ class Component(object):
     def __str__(self):
         return str(self.__dict__)
 
+    def get_from_page(self, file_name, full_path):
+        with open(file_name, "r") as file:
+            tag = html.document_fromstring(file.read()).xpath(full_path.xpath)[0]
+        if full_path.attr == "href" or full_path.attr == "title":
+            return tag.get(full_path.attr)
+        text = ""
+        for i in tag.itertext():
+            text += i
+        return text
+
+    def get_substitution(self, file_name):
+        subst = dict()
+        subst["type"] = self.type
+        subst["alignment"] = self.alignment
+        subst["page_url"] = self.get_from_page(file_name, self.page_url)
+        subst["title"] = self.get_from_page(file_name, self.title)
+        return subst
+
 
 class SearchResult(Component):
     def __init__(self):
@@ -25,8 +46,12 @@ class SearchResult(Component):
         self.snippet = None
         self.view_url = None
 
-    def __repr__(self):
-        pass
+    def get_substitution(self, file_name):
+        subst = Component.get_substitution(self, file_name)
+        subst["type"] = self.type
+        subst["snippet"] = self.get_from_page(file_name, self.snippet)
+        subst["view_url"] = self.get_from_page(file_name, self.view_url)
+        return subst
 
 
 class WizardImage(Component):
@@ -35,6 +60,15 @@ class WizardImage(Component):
         self.type = "WIZARD"
         self.wizard_type = "WIZARD_IMAGE"
         self.media_links = list()
+
+    def get_substitution(self, file_name):
+        subst = Component.get_substitution(self, file_name)
+        subst["type"] = self.type
+        subst["wizard_type"] = self.wizard_type
+        subst["media_links"] = list()
+        for img in self.media_links:
+            subst["media_links"].append(self.get_from_page(file_name, img))
+        return subst
 
 
 class Markup(object):
