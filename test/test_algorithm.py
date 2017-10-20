b1ee51c958e4b2e6f7697160f4efcab3fdf9fecd
kormyshov <kormyshov@gmail.com>
2017-10-20 13:18:55 +0300

    Fix after change to return analysis

--- test/test_algorithm.py
+++ test/test_algorithm.py
@@ -10,25 +10,25 @@ class TestAlgorithm(unittest.TestCase):
         markup_list = []
         markup_list.append(google_parser.parse_page("../google/2/1.html"))
         markup_list.append(google_parser.parse_page("../google/2/2.html"))
-        result_markup = algorithm.parse_page("../google/2/3.html", markup_list)
+        analysis = algorithm.parse_page("../google/2/3.html", markup_list)
 
-        self.assertEqual(result_markup.file, "../google/2/3.html")
+        self.assertEqual(analysis.file, "../google/2/3.html")
 
     def test_count_of_blocks_on_page3(self):
         markup_list = []
         markup_list.append(google_parser.parse_page("../google/2/1.html"))
         markup_list.append(google_parser.parse_page("../google/2/2.html"))
-        result_markup = algorithm.parse_page("../google/2/3.html", markup_list)
+        analysis = algorithm.parse_page("../google/2/3.html", markup_list)
 
-        self.assertEqual(len(result_markup.components), 9)
+        self.assertEqual(len(analysis.components), 9)
 
     def test_fields_of_first_result_search_on_page3(self):
         markup_list = []
         markup_list.append(google_parser.parse_page("../google/2/1.html"))
         markup_list.append(google_parser.parse_page("../google/2/2.html"))
-        result_markup = algorithm.parse_page("../google/2/3.html", markup_list)
+        analysis = algorithm.parse_page("../google/2/3.html", markup_list)
 
-        search_result = result_markup.get_substitution(0)
+        search_result = analysis.components[0]
         expected_component = Component()
         expected_component.type = "SEARCH_RESULT"
         expected_component.alignment = "LEFT"
@@ -42,33 +42,33 @@ class TestAlgorithm(unittest.TestCase):
         markup_list = []
         markup_list.append(google_parser.parse_page("../google/2/1.html"))
         markup_list.append(google_parser.parse_page("../google/2/2.html"))
-        result_markup = algorithm.parse_page("../google/2/3.html", markup_list)
+        analysis = algorithm.parse_page("../google/2/3.html", markup_list)
 
-        self.assertEqual(len(result_markup.components[0].media_links), 6)
+        self.assertEqual(len(analysis.components[0].media_links), 6)
 
     def test_title_of_wizard_image_on_page3(self):
         markup_list = []
         markup_list.append(google_parser.parse_page("../google/2/1.html"))
         markup_list.append(google_parser.parse_page("../google/2/2.html"))
-        result_markup = algorithm.parse_page("../google/2/3.html", markup_list)
+        analysis = algorithm.parse_page("../google/2/3.html", markup_list)
 
-        self.assertEqual(result_markup.get_substitution(0).title, "Картинки по запросу парашют")
+        self.assertEqual(analysis.components[0].title, "Картинки по запросу парашют")
 
     def test_page_url_of_wizard_image_on_page3(self):
         markup_list = []
         markup_list.append(google_parser.parse_page("../google/2/1.html"))
         markup_list.append(google_parser.parse_page("../google/2/2.html"))
-        result_markup = algorithm.parse_page("../google/2/3.html", markup_list)
+        analysis = algorithm.parse_page("../google/2/3.html", markup_list)
 
-        self.assertEqual(result_markup.get_substitution(0).page_url, "https://www.google.ru/search?q=%D0%BF%D0%B0%D1%80%D0%B0%D1%88%D1%8E%D1%82&newwindow=1&dcr=0&tbm=isch&tbo=u&source=univ&sa=X&ved=0ahUKEwi5_bz9tu3WAhWJApoKHVbrCgwQsAQIfA")
+        self.assertEqual(analysis.components[0].page_url, "https://www.google.ru/search?q=%D0%BF%D0%B0%D1%80%D0%B0%D1%88%D1%8E%D1%82&newwindow=1&dcr=0&tbm=isch&tbo=u&source=univ&sa=X&ved=0ahUKEwi5_bz9tu3WAhWJApoKHVbrCgwQsAQIfA")
 
     def test_media_links_on_page3(self):
         markup_list = []
         markup_list.append(google_parser.parse_page("../google/2/1.html"))
         markup_list.append(google_parser.parse_page("../google/2/2.html"))
-        result_markup = algorithm.parse_page("../google/2/3.html", markup_list)
+        analysis = algorithm.parse_page("../google/2/3.html", markup_list)
 
-        media_links = result_markup.get_substitution(0).media_links
+        media_links = analysis.components[0].media_links
         expected_list = ["http://www.happiness-shop.ru/parachute/construction-parachute.html",
                          "http://waterfuns.ru/shop/parashuty/",
                          "http://dic.academic.ru/dic.nsf/enc_tech/828/%D0%BF%D0%B0%D1%80%D0%B0%D1%88%D1%8E%D1%82",
