83bae1c665908131dbd95d211110323378794a3d
kormyshov <kormyshov@gmail.com>
2017-10-20 15:07:44 +0300

    Add JSON serialize

--- src/analysis.py
+++ src/analysis.py
@@ -1,3 +1,11 @@
+import json
+
+
+class Encoder(json.JSONEncoder):
+    def default(self, o):
+        return o.__dict__
+
+
 class Component(object):
     def __init__(self):
         self.type = None
@@ -6,7 +14,7 @@ class Component(object):
         self.title = None
 
     def __str__(self):
-        return str(self.__dict__)
+        return json.dumps(self, cls=Encoder, ensure_ascii=False)
 
     def __eq__(self, other):
         return self.__dict__ == other.__dict__
@@ -16,6 +24,9 @@ class Analysis(object):
     def __init__(self):
         self.file = None
         self.components = list()
+
+    def __str__(self):
+        return json.dumps(self, cls=Encoder, ensure_ascii=False)
 
     def add(self, component):
         self.components.append(component)
