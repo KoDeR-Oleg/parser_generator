26d36f75decfa71a530a59601515bb46d1cd87b8
kormyshov <kormyshov@gmail.com>
2017-11-03 02:56:16 +0300

    Add costs for operations

--- src/metrics/parser_result_levenstein_metric.py
+++ src/metrics/parser_result_levenstein_metric.py
@@ -3,5 +3,10 @@ from metrics.levenstein_metric import LevensteinMetric
 
 
 class ParserResultLevensteinMetric(Metric):
+    def __init__(self, add=1, delete=1, change=1):
+        self.add_cost = add
+        self.del_cost = delete
+        self.change_cost = change
+
     def distance(self, pr1, pr2):
-        return LevensteinMetric().distance(pr1.components, pr2.components)
+        return LevensteinMetric(self.add_cost, self.del_cost, self.change_cost).distance(pr1.components, pr2.components)
