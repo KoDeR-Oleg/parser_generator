14359f8039e42213035443d10bfe19e8e655aec9
kormyshov <kormyshov@gmail.com>
2017-11-03 04:55:24 +0300

    Add quality control for algorithm v2.0

--- src/main.py
+++ src/main.py
@@ -1,5 +1,6 @@
 from aggregators.avg_aggregator import AvgAggregator
 from algorithms.primitive_algorithm import PrimitiveAlgorithm
+from algorithms.primitive_algorithm_v2 import PrimitiveAlgorithm_v2
 from metrics.parser_result_levenstein_metric import ParserResultLevensteinMetric
 from quality_control import QualityControl
 
@@ -9,3 +10,6 @@ aggregator = AvgAggregator()
 algorithm = PrimitiveAlgorithm()
 control = QualityControl(metric, aggregator)
 print("Quality =", control.get_quality(algorithm))
+
+algorithm2 = PrimitiveAlgorithm_v2()
+print("Quality =", control.get_quality(algorithm2))
