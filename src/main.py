e4d395cbc5871a0b22d32f9276041693c7f05fbb
kormyshov <kormyshov@gmail.com>
2017-11-03 16:09:35 +0300

    Add algorithm3

--- src/main.py
+++ src/main.py
@@ -1,15 +1,19 @@
 from aggregators.avg_aggregator import AvgAggregator
 from algorithms.primitive_algorithm import PrimitiveAlgorithm
 from algorithms.primitive_algorithm_v2 import PrimitiveAlgorithm_v2
+from algorithms.primitive_algorithm_v3 import PrimitiveAlgorithm_v3
 from metrics.parser_result_levenstein_metric import ParserResultLevensteinMetric
 from quality_control import QualityControl
 
 metric = ParserResultLevensteinMetric()
 aggregator = AvgAggregator()
+control = QualityControl(metric, aggregator)
 
 algorithm = PrimitiveAlgorithm()
-control = QualityControl(metric, aggregator)
 print("Quality =", control.get_quality(algorithm))
 
 algorithm2 = PrimitiveAlgorithm_v2()
 print("Quality =", control.get_quality(algorithm2))
+
+algorithm3 = PrimitiveAlgorithm_v3()
+print("Quality =", control.get_quality(algorithm3))
