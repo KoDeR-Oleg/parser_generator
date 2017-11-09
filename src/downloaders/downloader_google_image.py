9b592546d57b28ed29a9c521a632f329f1a6af93
kormyshov <kormyshov@gmail.com>
2017-11-09 05:25:09 +0300

    Change downloader for GoogleImageParser

--- src/downloaders/downloader_google_image.py
+++ src/downloaders/downloader_google_image.py
@@ -1,5 +1,5 @@
 import requests
-from parsers.google_parser_v2 import GoogleParser_v2
+from parsers.google_image_parser import GoogleImageParser
 
 url = "https://www.google.com/search?q="
 suf_url = "&newwindow=1&dcr=0&source=lnms&tbm=isch&sa=X&ved=0ahUKEwiP5YHkqLDXAhWKJ5oKHUUmAh8Q_AUICygC&biw=1366&bih=702"
@@ -59,16 +59,15 @@ for i in range(len(request_list)):
 
     full_url = url + request_list[i] + suf_url
     file_name = "../../golden/google_image/" + str(i)
-
+    """
     response = requests.get(full_url)
     with open(file_name + ".html", "w") as file:
         file.write(response.text)
     """
     print("i =", i)
-    parser = GoogleParser_v2()
+    parser = GoogleImageParser()
     with open(file_name + ".json", "w") as file:
         with open(file_name + ".html", "r") as input:
             file.write(str(parser.parse(input.read())))
     with open(file_name + "_markup.json", "w") as file:
         file.write(str(parser.extract_markup(file_name + ".html")))
-    """
