51a7bab76596c849d7fc8e3d39d479263de3f88f
kormyshov <kormyshov@gmail.com>
2017-11-17 00:04:18 +0300

    Add ADV blocks

--- src/parsers/yandex_parser_v3.py
+++ src/parsers/yandex_parser_v3.py
@@ -1,11 +1,11 @@
 from lxml import html
 
-from markups.search_markup import SearchMarkupSearchResult, FullPath, SearchMarkup, SearchMarkupWizardImage, SearchMarkupWizardNews
+from markups.search_markup import SearchMarkupSearchResult, FullPath, SearchMarkup, SearchMarkupWizardImage, SearchMarkupWizardNews, SearchMarkupAdv
 from parser_result import ParserResult, Component
 from parsers.parser import Parser
 
 
-class YandexParser(Parser):
+class YandexParser_v3(Parser):
     @staticmethod
     def get_index(parent, tag):
         index = 1
@@ -20,7 +20,7 @@ class YandexParser(Parser):
     def get_path(element):
         path = ""
         while element.tag != "html":
-            path = "/" + element.tag + "[" + str(YandexParser.get_index(element.getparent(), element)) + "]" + path
+            path = "/" + element.tag + "[" + str(YandexParser_v3.get_index(element.getparent(), element)) + "]" + path
             element = element.getparent()
         path = "//html" + path
         return path
@@ -43,11 +43,20 @@ class YandexParser(Parser):
     def extract_search_result(self, element):
         search_result = SearchMarkupSearchResult()
         search_result.alignment = "LEFT"
-        search_result.page_url = FullPath(YandexParser.get_path(element) + "/h2/a", "href")
-        search_result.title = FullPath(YandexParser.get_path(element) + "/h2/a", "string")
-        search_result.snippet = FullPath(YandexParser.get_path(element) + "/div[2]/div[1]", "string")
-        search_result.view_url = FullPath(YandexParser.get_path(element) + "/div/div[1]/a[last()]", "href")
+        search_result.page_url = FullPath(YandexParser_v3.get_path(element) + "/h2/a", "href")
+        search_result.title = FullPath(YandexParser_v3.get_path(element) + "/h2/a", "string")
+        search_result.snippet = FullPath(YandexParser_v3.get_path(element) + "/div[2]/div[1]", "string")
+        search_result.view_url = FullPath(YandexParser_v3.get_path(element) + "/div/div[1]/a[last()]", "href")
         return search_result
+
+    def extract_adv(self, element):
+        adv = SearchMarkupAdv()
+        adv.alignment = "LEFT"
+        adv.page_url = FullPath(YandexParser_v3.get_path(element) + "/h2/a", "href")
+        adv.title = FullPath(YandexParser_v3.get_path(element) + "/h2/a", "string")
+        adv.snippet = FullPath(YandexParser_v3.get_path(element) + "/div[@class='organic__content-wrapper clearfix']/div[1]", "string")
+        adv.view_url = FullPath(YandexParser_v3.get_path(element) + "/div[@class='organic__subtitle typo typo_type_greenurl']/div[1]/a", "string")
+        return adv
 
     def parse_search_result(self, element):
         search_result = Component()
@@ -59,14 +68,24 @@ class YandexParser(Parser):
         search_result.view_url = self.get_from_page(element, "./div/div[1]/a[last()]", "href")
         return search_result
 
+    def parse_adv(self, element):
+        adv = Component()
+        adv.type = "ADV"
+        adv.alignment = "LEFT"
+        adv.page_url = self.get_from_page(element, "./h2/a", "href")
+        adv.title = self.get_from_page(element, "./h2/a", "string")
+        adv.snippet = self.get_from_page(element, "./div[@class='organic__content-wrapper clearfix']/div[1]", "string")
+        adv.view_url = self.get_from_page(element, "./div[@class='organic__subtitle typo typo_type_greenurl']/div[1]/a", "string")
+        return adv
+
     def extract_wizard_image(self, element):
         wizard = SearchMarkupWizardImage()
         wizard.alignment = "LEFT"
         img_list = element.xpath("./div[2]/div/div/div/a")
         for img in img_list:
-            wizard.media_links.append(FullPath(YandexParser.get_path(img) + "/div[1]/div[1]", "style"))
-        wizard.page_url = FullPath(YandexParser.get_path(element) + "/div[1]/h2/a", "href")
-        wizard.title = FullPath(YandexParser.get_path(element) + "/div[1]/h2/a", "string")
+            wizard.media_links.append(FullPath(YandexParser_v3.get_path(img) + "/div[1]/div[1]", "style"))
+        wizard.page_url = FullPath(YandexParser_v3.get_path(element) + "/div[1]/h2/a", "href")
+        wizard.title = FullPath(YandexParser_v3.get_path(element) + "/div[1]/h2/a", "string")
         return wizard
 
     def parse_wizard_image(self, element):
@@ -85,8 +104,8 @@ class YandexParser(Parser):
     def extract_wizard_news(self, element):
         wizard = SearchMarkupWizardNews()
         wizard.alignment = "LEFT"
-        wizard.page_url = FullPath(YandexParser.get_path(element) + "/div[1]/h2/a[2]", "href")
-        wizard.title = FullPath(YandexParser.get_path(element) + "/div[1]/h2/a[2]", "string")
+        wizard.page_url = FullPath(YandexParser_v3.get_path(element) + "/div[1]/h2/a[2]", "href")
+        wizard.title = FullPath(YandexParser_v3.get_path(element) + "/div[1]/h2/a[2]", "string")
         return wizard
 
     def parse_wizard_news(self, element):
@@ -107,6 +126,8 @@ class YandexParser(Parser):
         for block in block_list:
             if (len(block.xpath("./div[2]/div[2]")) > 0 and block.xpath("./div[2]/div[2]")[0].text == "реклама") or \
                     (len(block.xpath("./div[1]/div[2]")) > 0 and block.xpath("./div[1]/div[2]")[0].text == "реклама"):
+                result = self.extract_adv(block)
+                markup.add(result)
                 continue
             if len(block.xpath("./h2/a")) > 0 and \
                     not (len(block.getparent().xpath("./div[2]")) > 0 and len(block.getparent().xpath("./div[2]")[0].cssselect("div.video2")) > 0):
@@ -127,6 +148,8 @@ class YandexParser(Parser):
         for block in block_list:
             if (len(block.xpath("./div[2]/div[2]")) > 0 and block.xpath("./div[2]/div[2]")[0].text == "реклама") or \
                     (len(block.xpath("./div[1]/div[2]")) > 0 and block.xpath("./div[1]/div[2]")[0].text == "реклама"):
+                result = self.parse_adv(block)
+                parser_result.add(result)
                 continue
             if len(block.xpath("./h2/a")) > 0 and \
                     not (len(block.getparent().xpath("./div[2]")) > 0 and len(block.getparent().xpath("./div[2]")[0].cssselect("div.video2")) > 0):
