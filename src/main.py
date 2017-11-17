5110f5ee5572589a65b576fb9194d166aa1bc89b
kormyshov <kormyshov@gmail.com>
2017-11-17 10:59:30 +0300

    Fix for v2

--- src/main.py
+++ src/main.py
@@ -3,6 +3,7 @@ from algorithms.primitive_algorithm import PrimitiveAlgorithm
 from algorithms.primitive_algorithm_v2 import PrimitiveAlgorithm_v2
 from algorithms.primitive_algorithm_v3 import PrimitiveAlgorithm_v3
 from algorithms.algorithm_v1 import Algorithm_v1
+from algorithms.algorithm_v2 import Algorithm_v2
 from metrics.parser_result_levenstein_metric import ParserResultLevensteinMetric
 from quality_control import QualityControl
 
@@ -10,8 +11,8 @@ metric = ParserResultLevensteinMetric()
 aggregator = AvgAggregator()
 control = QualityControl(metric, aggregator)
 
-#algorithm3 = PrimitiveAlgorithm_v3()
-#print("Quality =", control.get_quality(algorithm3))
+algorithm1 = Algorithm_v1("")
+print("Quality =", control.get_quality(algorithm1))
 
-algorithm = Algorithm_v1("")
-print("Quality =", control.get_quality(algorithm))
+algorithm2 = Algorithm_v2("")
+print("Quality =", control.get_quality(algorithm2))
