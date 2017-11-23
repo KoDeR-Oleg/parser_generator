674d0064724e7fe17c82e674def0804e7aaf5369
kormyshov <kormyshov@gmail.com>
2017-11-24 00:32:44 +0300

    Refactoring HTMLPath

--- src/markups/markup.py
+++ src/markups/markup.py
@@ -1,40 +1,4 @@
-from abc import ABCMeta, abstractmethod, abstractstaticmethod, abstractproperty
-
-
-class TreePath:
-    __metaclass__ = ABCMeta
-
-    @abstractmethod
-    def get_value(self, root):
-        "Получение значения из дерева"
-
-    @abstractmethod
-    def get_relative_path(self, tree_path):
-        "Получение пути относительно tree_path"
-
-    @abstractmethod
-    def get_common_prefix(self, tree_path):
-        "Получение наибольшего общего пути"
-
-    @abstractmethod
-    def get_elements(self, tree):
-        "Получение всех элементов для заданного пути"
-
-    @abstractmethod
-    def len(self):
-        "Получение длины пути"
-
-    @abstractmethod
-    def drop_for_len(self, len):
-        "Обрезание пути до заданной длины"
-
-    @abstractmethod
-    def concat(self, tree_path):
-        "Конкатенация путей"
-
-    @abstractstaticmethod
-    def get_tree(raw_page):
-        "Получение дерева путей"
+from abc import ABCMeta, abstractstaticmethod
 
 
 class Markup:
--- src/markups/search_markup.py
+++ src/markups/search_markup.py
@@ -1,108 +1,8 @@
 import jsonpickle
-from markups.markup import Markup, TreePath
+
+from markups.markup import Markup
 from parser_result import Component
-from lxml import html
-
-
-class FullPath(TreePath):
-    def __init__(self, xpath, attr):
-        self.xpath = xpath
-        self.attr = attr
-
-    def __eq__(self, other):
-        return self.__dict__ == other.__dict__
-
-    def __str__(self):
-        return self.xpath + "." + self.attr
-
-    def get_value(self, tree):
-        tags = tree.xpath(self.xpath)
-        if len(tags) == 0:
-            return None
-        else:
-            tag = tags[0]
-        attrs = ["href", "title", "style", "src"]
-        if self.attr in attrs:
-            if self.attr == "style":
-                if len(tag.get("style").split("//")) > 1:
-                    return tag.get("style").split("//")[1][:-2]
-                else:
-                    return None
-            return tag.get(self.attr)
-        return tag.text_content()
-
-    def split_xpath(self):
-        extract_list = self.xpath.split("/")
-        while extract_list[0] == "":
-            extract_list = extract_list[1:]
-        for i in range(len(extract_list)):
-            if extract_list[i][-1] != "]":
-                extract_list[i] = (extract_list[i], "0")
-            else:
-                tlist = extract_list[i].split("[")
-                extract_list[i] = (tlist[0], tlist[1][:-1])
-        return extract_list
-
-    def merge_xpath(self, extract_list, relative=False):
-        xpath = ""
-        if relative:
-            xpath = "."
-        if len(extract_list) > 0 and extract_list[0][0] == "html":
-            xpath = "/"
-        if len(extract_list) > 0 and extract_list[0][0] == ".":
-            xpath = "."
-            extract_list = extract_list[1:]
-        for item in extract_list:
-            xpath += "/" + item[0]
-            if str(item[1]) != "0":
-                xpath += "[" + str(item[1]) + "]"
-        return xpath
-
-    def get_common_prefix(self, tree_path, in_block=False):
-        xpath1 = self.split_xpath()
-        xpath2 = tree_path.split_xpath()
-        common_list = []
-        for i in range(min(len(xpath1), len(xpath2))):
-            if xpath1[i][0] != xpath2[i][0]:
-                break
-            if xpath1[i][1] == xpath2[i][1]:
-                common_list.append(xpath1[i])
-            else:
-                if in_block:
-                    break
-                common_list.append((xpath1[i][0], 0))
-        xpath = self.merge_xpath(common_list)
-        attr = self.attr
-        if attr != tree_path.attr:
-            attr = None
-        return FullPath(xpath, attr)
-
-    def get_relative_path(self, tree_path):
-        return FullPath(self.merge_xpath(self.split_xpath()[len(tree_path.split_xpath()):], True), self.attr)
-
-    def get_elements(self, tree):
-        return tree.xpath(self.xpath)
-
-    def len(self):
-        lst = self.split_xpath()
-        if len(lst) == 1 and lst[0][0] == ".":
-            return 0
-        return len(lst)
-
-    def drop_for_len(self, len):
-        return FullPath(self.merge_xpath(self.split_xpath()[:len]), None)
-
-    def concat(self, tree_path):
-        result = FullPath(self.xpath, tree_path.attr)
-        if tree_path.xpath[0] == '.':
-            result.xpath += tree_path.xpath[1:]
-        else:
-            result.xpath += tree_path.xpath
-        return result
-
-    @staticmethod
-    def get_tree(raw_page):
-        return html.fromstring(raw_page)
+from trees.html_path import HTMLPath
 
 
 class SearchMarkupComponent(Component):
@@ -178,4 +78,4 @@ class SearchMarkup(Markup):
 
     @staticmethod
     def get_TreePath_class():
-        return FullPath
+        return HTMLPath
