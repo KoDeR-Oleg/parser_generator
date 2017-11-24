1af29f63b73cae3bfa70b84a0fd0637d844801dd
kormyshov <kormyshov@gmail.com>
2017-11-24 06:56:14 +0300

    Add two types selectors

--- src/main.py
+++ src/main.py
@@ -4,6 +4,8 @@ from algorithms.primitive_algorithm_v2 import PrimitiveAlgorithm_v2
 from algorithms.primitive_algorithm_v3 import PrimitiveAlgorithm_v3
 from algorithms.algorithm_v1 import Algorithm_v1
 from algorithms.algorithm_v2 import Algorithm_v2
+from algorithms.selectors.simple_selector import SimpleSelector
+from algorithms.selectors.black_list_selector import BlackListSelector
 from metrics.parser_result_levenstein_metric import ParserResultLevensteinMetric
 from quality_control import QualityControl
 
@@ -12,7 +14,12 @@ aggregator = AvgAggregator()
 control = QualityControl(metric, aggregator)
 
 algorithm1 = Algorithm_v1("")
-print("Quality =", control.get_quality(algorithm1))
+print("Algorithm v1.: quality =", control.get_quality(algorithm1))
 
-algorithm2 = Algorithm_v2("")
-print("Quality =", control.get_quality(algorithm2))
+simple_selector = SimpleSelector()
+algorithm2 = Algorithm_v2("", selector=simple_selector)
+print("Algorithm v2. SimpleSelector: quality =", control.get_quality(algorithm2))
+
+blacklist_selector = BlackListSelector()
+algorithm3 = Algorithm_v2("", selector=blacklist_selector)
+print("Algorithm v2. BlackListSelector: quality =", control.get_quality(algorithm3))
