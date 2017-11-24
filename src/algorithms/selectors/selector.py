7996a7cdd6733f2ade8883552c3ca8c724747f66
kormyshov <kormyshov@gmail.com>
2017-11-24 06:16:06 +0300

    Init selector interface

---
+++ src/algorithms/selectors/selector.py
@@ -0,0 +1,15 @@
+from abc import ABCMeta, abstractmethod
+from algorithms.algorithm import Algorithm
+from markups.markup import Markup
+from typing import List
+
+class Selector:
+    __metaclass__ = ABCMeta
+
+    @abstractmethod
+    def __init__(self, algorithm: Algorithm, markup_list: List[Markup]):
+        "Инициализация фильтра коллизий"
+
+    @abstractmethod
+    def get_iter(self, **kwargs):
+        "Получение итератора по компонентам дерева с учётом фильтра"
