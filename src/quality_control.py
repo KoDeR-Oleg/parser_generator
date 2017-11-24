7f2802c8f024a59b08ed9bdf8b71bc75084137a5
kormyshov <kormyshov@gmail.com>
2017-11-24 04:03:40 +0300

    Add test for json

--- src/quality_control.py
+++ src/quality_control.py
@@ -1,10 +1,35 @@
 from parsers.ideal_parser import IdealParser
+import jsonpickle
 
 
 class QualityControl(object):
     def __init__(self, metric, aggregator):
         self.metric = metric
         self.aggregator = aggregator
+
+    def cv_json(self, algorithm, path, part=0.5):
+        N = 50
+        ideal_parser = IdealParser(path)
+
+        learn_nums = range(int(N * part))
+        test_nums = range(int(N * part), N)
+
+        markup_list = list()
+        for i in learn_nums:
+            markup_list.append(ideal_parser.extract_markup(str(i) + "_markup.json"))
+
+        algorithm.directory = path
+        algorithm.learn(markup_list)
+        dist = list()
+        for i in test_nums:
+            with open(path + str(i) + ".json", "r") as file:
+                string = file.read()
+            parser_result = algorithm.parse(string)
+            with open(path + str(i) + "_result.json", "r") as file:
+                ideal_result = jsonpickle.decode(file.read())
+            dist.append(self.metric.distance(parser_result, ideal_result))
+
+        return self.aggregator.aggregate(dist)
 
     def cv(self, algorithm, path, part=0.5):
         N = 50
@@ -34,5 +59,6 @@ class QualityControl(object):
         result['google'] = self.cv(algorithm, "../golden/google/")
         result['google_image'] = self.cv(algorithm, "../golden/google_image/")
         result['yandex'] = self.cv(algorithm, "../golden/yandex/")
+        result['wiki'] = self.cv_json(algorithm, "../golden/wiki/")
         result['total'] = self.aggregator.aggregate(result.values())
         return result
