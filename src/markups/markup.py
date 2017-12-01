a97173a4cc0932aba37955d1c3f928d6fc9f3fb0
kormyshov <kormyshov@gmail.com>
2017-12-01 07:45:03 +0300

    Delete abstract static method

--- src/markups/markup.py
+++ src/markups/markup.py
@@ -3,7 +3,3 @@ from abc import ABCMeta, abstractstaticmethod
 
 class Markup:
     __metaclass__ = ABCMeta
-
-    @abstractstaticmethod
-    def get_TreePath_class():
-        "Получение класса для путей"
